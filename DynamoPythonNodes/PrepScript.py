f0042c8540e4c8eb157a3031bf6ed134db48d9d2
ge95xoq <mohab.hassaan@tum.de>
2023-05-16 14:39:24 +0200

    final prep script

--- DynamoPythonNodes/PrepScript.py
+++ DynamoPythonNodes/PrepScript.py
@@ -1,34 +1,47 @@
-## @brief Preprocesses input parameters
-# Takes user input (left side) as input 
-# Outputs filename (str) and input_params (list)
+## @brief Preprocesses dynamo script's input parameters
+# Takes user input (left side) as input and splits it into pre-refactoring and post-refactoring user parameters
+# @In array of user input parameters
+# @OUT Tuple of 
+# A text file's name containing all pre-refactoring user parameters as part of the name
+# List of all the pre-refactoring user parameters in their original type
+# List of all the post-refactoring user parameters in their original type
 
 # Constants...
 PRE_FACTORING_INPUT_LENGTH = 6
 BOTTLENECK_INDEX = 5
 
+## 4th index left empty to be consistent with pre-refactoring file name
+INPUT_NAMES = [
+    "siteX" , "siteY" , "CORRWIDTH" , "NUMROOMS" , "" , "INCBNECK"
+]
+
 # Helper functions...
 
 ## Converts a string to an integer
+# @param int_as_string:str string to be converted
+# @returns argument converted to an integer
 # @raises ValueError
 def convert_to_int(int_as_string) : 
     try : 
         return int(int_as_string)
     except : 
-        raise ValueError("Argument string cannot be converted to integer")
-
-## Converts an integer to a string
-def convert_to_string(string_as_int) : 
-    return str(string_as_int)
+        raise ValueError(f"{int_as_string} cannot be converted to integer")
 
 ## Converts str to boolean : 
+# @param bool_as_string:str string to be converted
+# @returns argument converted to a boolean
+# @note converted to integer first to counter edge case: "0" is casted to True
 # @raises ValueError
 def convert_to_bool(bool_as_string) : 
     try : 
-        return bool(int(bool_as_string))
-    except : 
-        raise ValueError("Argument string cannot be converted to boolean")
+        return bool(convert_to_int(bool_as_string))
+    except ValueError as e : 
+        if "False" in str(e) or "True" in str(e) : return bool(bool_as_string)
+        else : raise ValueError("Argument string cannot be converted to boolean")
     
 ## Converts str to float : 
+# @param float_as_string:str string to be converted
+# @returns argument converted to a float
 # @raises ValueError
 def convert_to_float(float_as_string) : 
     try : 
@@ -57,24 +70,29 @@ def convert_input_from_string(input_as_string , include_bottleneck_index) :
     ]
 
 ## Generates name of the text file from user input parameters
+# Adds pairs of names and parameters to a string and then returns string
+# @param user_input:list values of input parameters to be included in file name
+# @param input_names:list<str> names of input parameters to be included in file name
 # @returns filename:str final filename
-def generate_file_name(user_input) : 
-    input_names = [
-        "siteX" , "siteY" , "CORRWIDTH" , "NUMROOMS" , "" , "INCBNECK"
-    ]
+# @raises ValueError
+def generate_file_name(user_input , input_names) : 
+    if len(user_input) != len(input_names) : 
+        raise ValueError("Unequal list lengths")
 
     file_name = "floorplan"
 
     for i in range(len(user_input)) :
-        input_as_string = convert_to_string(user_input[i])
+        input_as_string = user_input[i]
         name_input_pair_as_string = f"_{input_names[i]}_{input_as_string}" if i != 4 else f"_{input_as_string}"
         file_name += name_input_pair_as_string
 
     file_name += ".txt"
 
     return file_name
-
+#-----------------------------------------------------------------------------------------------------------------#
 # Script...
 pre_factoring_user_input , additional_user_input = convert_input_from_string(IN , BOTTLENECK_INDEX)
 
-OUT = generate_file_name(pre_factoring_user_input) , pre_factoring_user_input , additional_user_input 
+OUT = generate_file_name(pre_factoring_user_input , INPUT_NAMES) , pre_factoring_user_input , additional_user_input
+
+print(OUT)
