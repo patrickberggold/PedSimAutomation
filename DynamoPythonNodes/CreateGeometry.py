deef906f04f5348ebd3245e42e02fd67e348c920
ge95xoq <mohab.hassaan@tum.de>
2023-05-18 11:12:35 +0200

    updated docs

--- DynamoPythonNodes/CreateGeometry.py
+++ DynamoPythonNodes/CreateGeometry.py
@@ -11,10 +11,11 @@ def convert_to_float(float_as_string) :
     except : 
         raise ValueError("Argument string cannot be converted to float")
 
-## Converts a list of values from meters to Revit units
+## Converts a value or list of values from meters to Revit units
+# Checks if arg is a single value, and converts it to Revit units
+# Otherwise, arg is a list and all values are converted in a loop
 # @param values_in_meter: list<float> list of values to be converted
 # @returns values_in_revit_units: list<float> list of values in Revit units
-# @note function works with a single float value as well, and returns a single value in that case
 # @raises ValueError
 def convert_to_revit_units(values_in_meter) : 
     if values_in_meter == None : 
