e81fcc4c273ca0da0619ce66cfb38cb3aec4f032
Mohab Hassaan <mohab998@gmail.com>
2023-06-15 17:51:00 +0200

    color script backup

--- DynamoDependencies/colorDirectly/coloredfloor.py
+++ DynamoDependencies/colorDirectly/coloredfloor.py
@@ -8,14 +8,14 @@ from RevitServices.Transactions import TransactionManager
 
 doc = DocumentManager.Instance.CurrentDBDocument
 
-def slab_geometry_by_level(x, y, level, delta_z):
+def slab_geometry_by_level(x, y, level, delta_z , start_x , start_y):
     zz = level.Elevation + delta_z
     xx = x
     yy = y
-    line_1 = Line.CreateBound(XYZ(0, 0, zz), XYZ(xx, 0, zz))
-    line_2 = Line.CreateBound(XYZ(xx, 0, zz), XYZ(xx, yy, zz))
-    line_3 = Line.CreateBound(XYZ(xx, yy, zz), XYZ(0, yy, zz))
-    line_4 = Line.CreateBound(XYZ(0, yy, zz), XYZ(0, 0, zz))
+    line_1 = Line.CreateBound(XYZ(start_x, start_y, zz), XYZ(xx, start_y, zz))
+    line_2 = Line.CreateBound(XYZ(xx, start_y, zz), XYZ(xx, yy, zz))
+    line_3 = Line.CreateBound(XYZ(xx, yy, zz), XYZ(start_x, yy, zz))
+    line_4 = Line.CreateBound(XYZ(start_x, yy, zz), XYZ(start_x, start_y, zz))
 
     slab_geometry = CurveArray()
     slab_geometry.Append(line_1)
@@ -30,39 +30,39 @@ def OverrideColorPattern(element, color, pattern, view):
     graphicSettings.SetCutForegroundPatternColor(color)
     graphicSettings.SetSurfaceForegroundPatternId(UnwrapElement(pattern).Id)
     graphicSettings.SetCutForegroundPatternId(UnwrapElement(pattern).Id)
+    graphicSettings.SetSurfaceBackgroundPatternId(UnwrapElement(pattern).Id)
+    graphicSettings.SetCutBackgroundPatternId(UnwrapElement(pattern).Id)
+    graphicSettings.SetProjectionLineColor(color)
     UnwrapElement(view).SetElementOverrides(element.Id, graphicSettings)
 
-# Start a transaction
-TransactionManager.Instance.EnsureInTransaction(doc)
+if 0 : 
+    TransactionManager.Instance.EnsureInTransaction(doc)
 
-# Define floor dimensions
-length = 5.0
-width = 5.0
+    length = 100.
+    width = 70.
+    start_x = 20.
+    start_y = 20.
 
-# Get the level to place the floor on
-level = doc.ActiveView.GenLevel
+    level = doc.ActiveView.GenLevel
 
-# Create floor geometry
-floor_geometry = slab_geometry_by_level(length, width, level, 1.)
+    floor_geometry = slab_geometry_by_level(length, width, level, 1. , start_x , start_y)
 
-# Create a floor
-floor = doc.Create.NewFloor(floor_geometry, True)
+    floor = doc.Create.NewFloor(floor_geometry, True)
 
-# Set the floor color
-red = Color(255, 0, 0)  # Solid red color
-fillPatterns = FilteredElementCollector(doc).OfClass(FillPatternElement)
-solidPattern = None
-for pattern in fillPatterns:
-    if UnwrapElement(pattern).GetFillPattern().IsSolidFill:
-        solidPattern = pattern
-        break
+    red = Color(255 , 0 , 0) 
+    fillPatterns = FilteredElementCollector(doc).OfClass(FillPatternElement)
+    solidPattern = None
+    for pattern in fillPatterns:
+        if UnwrapElement(pattern).GetFillPattern().IsSolidFill:
+            solidPattern = pattern
+            break
 
-view = doc.ActiveView
+    view = doc.ActiveView
 
-if solidPattern : 
-    OverrideColorPattern(floor, red, solidPattern, view)
+    if solidPattern : 
+        OverrideColorPattern(floor, red, solidPattern, view)
 
-    TransactionManager.Instance.TransactionTaskDone()
-    OUT = 0
-else : 
-    OUT = 1
+        TransactionManager.Instance.TransactionTaskDone()
+        OUT = 0
+    else : 
+        OUT = 1
